4364c4a78e2f69b1e4dff56563d02c3156338ff1
Ozan Turgut <oturgut@turn.com>
2012-12-18 11:06:03 -0800

    Added executable
    Signed-off-by: Ozan Turgut <oturgut@turn.com>

--- example/chance.js
+++ example/chance.js
@@ -1,34 +1,24 @@
-var winston = require("winston"),
-	logger = new (winston.Logger)({transports: [new (winston.transports.Console)({level: 'info'}) ]}),
-	createQueenClient = require("../").client;
+var startTime = (new Date()).getTime();
+var numberToFind = 42;
+var maxNumber = 100;
 
-var chanceExample = function(queen){
-	var startTime = (new Date()).getTime(),
-		maxNumber = 1000,
-		numberToFind = 42,
-		workforce = queen({
-			scriptPath: 'http://localhost/example/chance.js',
-			populate: "continuous",
-			handler: function(worker){
-				worker(maxNumber);
-			},
-			stop: function(){
-				console.log('Waiting for workers to connect...');
-			},
-			killOnStop: false
-		});
+var workforce = queen({
+	scripts: ['http://localhost/example/chance.js'],
+	populate: "continuous",
+	killOnStop: false,
+	handler: function(worker){
+		worker(maxNumber);
+	}
+});
 
-	workforce.on('message', function(number, worker){
-		console.log(number + " [" + worker.provider.attributes.name + "]");
-		if(number === 42){
-			var endTime = (new Date()).getTime(),
-				secondsToComplete = (endTime - startTime) / 1000;
+workforce.on('message', function(number, worker){
+	console.log(number + " (" + worker.provider.attributes.name + ")");
 
-			console.log('Done! That took ' + secondsToComplete + " seconds. The winner was " + worker.provider.attributes.name);
-			workforce.kill();
-			process.exit(0);
-		}
-	});
-};
-
-createQueenClient(chanceExample);
+	if(number === 42){
+		workforce.kill();	
+		var endTime = (new Date()).getTime();
+		var secondsToComplete = (endTime - startTime) / 1000;
+		console.log('Done! That took ' + secondsToComplete + " seconds. The winner was " + worker.provider.attributes.name);
+		process.exit(0);
+	}
+});
--- example/pingPong.js
+++ example/pingPong.js
@@ -1,24 +1,16 @@
-var winston = require("winston"),
-	logger = new (winston.Logger)({transports: [new (winston.transports.Console)({level: 'info'}) ]}),
-	createQueenClient = require("../").client;
-
-var pingPongExample = function(queen){
-	var workerHandler = function(worker){
-		worker.on('message', function(message){
-			console.log(message + " (" +  worker.provider.attributes.name + ")");
-			worker('pong');
-		});
-	};
-
-	var workforce = queen({
-		scriptPath: 'http://localhost/example/ping.js',
-		populate: "continuous",
-		stop: function(){
-			console.log("Waiting for someone to connect...");
-		},
-		killOnStop: false,
-		handler: workerHandler
+var workerHandler = function(worker){
+	worker.on('message', function(message){
+		console.log(message + " (" +  worker.provider.attributes.name + ")");
+		worker('pong');
 	});
 };
 
-createQueenClient(pingPongExample);
+var workforce = queen({
+	scriptPath: 'http://localhost/example/ping.js',
+	populate: "continuous",
+	stop: function(){
+		console.log("Waiting for someone to connect...");
+	},
+	killOnStop: false,
+	handler: workerHandler
+});
